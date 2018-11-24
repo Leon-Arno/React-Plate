f1a454dfdb22b1a9d7140799da8adbf8a835bb5e
Leon-Arno <leonarno.rimrod@gmail.com>
2018-11-24 23:16:57 +0100

    Finished basic file setup

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <Fragment>
+        <h1 className="text-dark text-center mt-4">React-Plate</h1>
+        <p className="text-center">A minimal plate</p>
+      </Fragment>
+    );
+  }
+}
+
+export default Header;
