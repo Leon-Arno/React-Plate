278c31c5caa15d51498a6cc1c08912ebd9dcec91
Leon-Arno <leonarno.rimrod@gmail.com>
2018-11-24 23:50:49 +0100

    Cleanup

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,13 @@ class Header extends Component {
   render() {
     return (
       <Fragment>
-        <h1 className="text-dark text-center mt-4">React-Plate</h1>
-        <p className="text-center">A minimal plate</p>
+        <div className="border mt-4 p-3">
+          <h1 className="text-dark text-center mt-4">React-Plate</h1>
+          <p className="text-center">
+            A small boilerplate to get you reacting to react.
+          </p>
+          <i className=" col-12 text-center fab fa-react fa-9x" />
+        </div>
       </Fragment>
     );
   }
